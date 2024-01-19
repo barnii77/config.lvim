89490084753c9e666aaf8534f21e8c74c008dd1b
barnii77 <david.harrer@gmx.at>
2024-01-19 19:34:19 +0100

    VIM BE GOOD

--- lua/barnii77/plugins.lua
+++ lua/barnii77/plugins.lua
@@ -131,21 +131,24 @@ local plugins = {
   {
     'nvim-lualine/lualine.nvim',
     config = function()
-     require("lualine").setup {
-      sections = {
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          }
+      require("lualine").setup {
+        sections = {
+          lualine_x = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            }
+          },
         },
-      },
-    }
+      }
     end
   },
   -- undotree
   "mbbill/undotree",
+
+  -- VimBeGood
+  "ThePrimeagen/vim-be-good",
 }
 
 return plugins
