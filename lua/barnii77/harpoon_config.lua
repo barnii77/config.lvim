15703302baa622d919f41b10b303b246f6804e00
barnii77 <david.harrer@gmx.at>
2024-02-06 02:09:02 +0100

    bad mappings for harpoon - added shift to most of them

--- lua/barnii77/harpoon_config.lua
+++ lua/barnii77/harpoon_config.lua
@@ -7,14 +7,14 @@ lvim.builtin.which_key.mappings["a"] = {
   "<cmd>HarpoonAppend<cr>", "Append to Harpoon"
 }
 -- NOTE: <C-l> is an alias for <Esc> in my config but only in insert and visual mode
-vim.keymap.set("n", "<C-l>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-S-l>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-S-k>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-S-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-S-h>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 vim.keymap.set("n", "<C-y>", function() harpoon:list():select(5) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-S-p>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-n>", function() harpoon:list():next() end)
